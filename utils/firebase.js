0cabd59059642a5c0251c40620559f451c84de3f
Emre <emresurbahan@yahoo.com>
2021-05-14 14:21:00 +0300

    config

--- utils/firebase.js
+++ utils/firebase.js
@@ -2,7 +2,14 @@ import * as firebase from "firebase";
 import "firebase/database";
 
 let config = {
- YOUR CONFIG,
+   apiKey: "AIzaSyAquxR7q7azZ3z1KeDR-D_H_Wc4H2PZnOw",
+   authDomain: "plus1-61415.firebaseapp.com",
+   projectId: "plus1-61415",
+   databaseURL:
+      "https://plus1-61415-default-rtdb.europe-west1.firebasedatabase.app/",
+   storageBucket: "plus1-61415.appspot.com",
+   messagingSenderId: "885951298573",
+   appId: "1:885951298573:web:5eefffd3876c11a72b6944",
 };
 
 if (!firebase.apps.length) {
